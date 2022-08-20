0dc8dccc9017e87836e7b58b0ad28050cbf67866
Anugya Baraik <83193763+Anugya12@users.noreply.github.com>
2022-08-20 14:52:22 +0530

    api fetching by Leher

--- movie database/script.js
+++ movie database/script.js
@@ -26,3 +26,44 @@ function close(){
     mainMenu.style.display = 'none';
    
 }
+
+
+
+//fetching api
+const API_KEY = '6c915d8cb8598277067b0da21d0c7cd4' ;
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_URL = BASE_URL + 'movie/popular?api_key=6c915d8cb8598277067b0da21d0c7cd4&language=en-US&page=1';
+const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
+const main = document.getElementById('content');
+
+
+GetMovies(API_URL);
+
+function GetMovies(url){
+    fetch(url).then(res => res.json()).then(data =>{
+        console.log(data.results);
+        ShowMovies(data.results);
+    })
+}
+
+function ShowMovies(data){
+    main.innerHTML = ``;
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview, genre_ids, release_date}= movie;
+        const movieElement = document.createElement('div');
+        movieElement.classList.add('cards');
+        movieElement.innerHTML = `
+        <img src="${IMG_URL+poster_path}" alt="${title}">
+            <div class="movieinfo">
+                <h3>${title}</h3>
+                <span class="rating">${vote_average}</span>
+                <h4>Genre</h4>
+                <span>${release_date}</span>
+            </div>
+            <div class="description">
+                ${overview}
+            </div>
+        `
+        main.appendChild(movieElement);
+    });
+}
